64e76d078a6da9140bc4d423bd9d9b1d2617a3f8
a.v.altunin <a.v.altunin@newton-technology.ru>
2023-12-16 12:20:26 +0300

    fix low coupling

--- src/main/java/org/example/newtonpatterns/service/util/MessageTemplateCreator.java
+++ src/main/java/org/example/newtonpatterns/service/util/MessageTemplateCreator.java
@@ -1,10 +1,13 @@
 package org.example.newtonpatterns.service.util;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageTemplateCreator {
 
     public String createTemplateByCode(String code) {
         return switch (code) {
-            case "1" -> "HAPPY BIRTHDAY";
+            case "1" -> "HAPPY BIRTHDAY TEMPLATE";
             case "2" -> "WELCOME TEMPLATE";
             case "3" -> "INFORMATION TEMPLATE";
             default -> "";
